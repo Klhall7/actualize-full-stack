afd729cdf7414e4b79fa177cb242107ab5268c69
Kyra Hall <kyravitch98@gmail.com>
2024-03-13 12:16:35 -0400

    working register/login backend

--- api/main.py
+++ api/main.py
@@ -29,14 +29,13 @@ supabase = create_supabase_client()
 def home():
     return{"message": "root route"}
 
-# need to add get logic for getting current user to do login 
 
 @app.post("/register")
 def register_user(request: User):
     response = supabase.auth.sign_up({
         "email": request.email, 
         "password": request.password, 
-    }) #redirects and response checks on frontend
+    }) #redirects on frontend 
     return response
 
 @app.post("/login")
@@ -47,25 +46,8 @@ def login_user(request: User):
         }) #redirects and status response checks on frontend
     return response
     
-@app.post("/logout")
+@app.post("/logout") 
 def logout_user():
     response = supabase.auth.sign_out()
-    #need to check current user first 
     return response
-    
-#     response_object = response()
-            # status_code = response_object.status_code
-        
-        
-    #     if status_code == 200:
-    #         return response
-        
-    #     # Check if the error is due to invalid credentials
-    #     if status_code == 401:
-    #         raise HTTPException(status_code=401, detail="Invalid credentials")
-    #     else:
-    #         raise HTTPException(status_code=status_code, detail="Unexpected Error")
-            
-    # except Exception as e:
-    #     # If an exception occurs, return HTTP 500 with the exception message
-    #     raise HTTPException(status_code=500, detail=str(e))
+    #should already have jwt token to end current session
