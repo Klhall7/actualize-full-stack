7eb30e65e30df61052ea6b8567077a7f907c3011
Kyra Hall <kyravitch98@gmail.com>
2024-03-13 22:19:21 -0400

    notes updated for tracking next steps

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
         <>
-            <Outlet id='outlet' />
-             {/* need to add global styling */}
-             {/* need to create footer that will load on every route */}
+            <Outlet id="outlet" />
+            {/* need to add global styling */}
+            {/* need to create footer that will load on every route */}
         </>
     );
-}
+};
 
-export default Layout
+export default Layout;
+
--- frontend/src/routes/DashboardPage.jsx
+++ frontend/src/routes/DashboardPage.jsx
@@ -1,6 +1,7 @@
 const DashboardPage = () => {
     return (
         <>
+            <h1>Dashboard</h1>
             <p>user default homepage at login</p>
             {/* need to import dashNav component*/}
             {/* need to set up menu for tasks */}
