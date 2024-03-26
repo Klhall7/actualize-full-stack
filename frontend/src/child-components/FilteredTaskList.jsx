f392a9d7cbecc9edabe84836c855921f66ae6935
Kyra Hall <kyravitch98@gmail.com>
2024-03-26 19:34:32 -0400

    working filter search with loading and error messages

---
+++ frontend/src/child-components/FilteredTaskList.jsx
@@ -0,0 +1,90 @@
+/* eslint-disable react/prop-types */
+import { format } from "date-fns";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import EditAndReturnTask from "./EditAndReturnTask";
+
+const FilteredTaskList = ({
+    tasks,
+    open,
+    setOpen,
+    setSelectedTask,
+    selectedTask,
+}) => {
+    if (!tasks) {
+        return <p>Loading tasks...</p>;
+    }
+    console.log("filtered tasks for list", tasks);
+    console.log("selected task state", selectedTask);
+
+    const handleClick = (task) => {
+        console.log("Edit Clicked, Prop Passed:", task);
+        console.log("selected task state", selectedTask);
+        setSelectedTask(task);
+        setOpen(true);
+    };
+
+    const handleCloseOnSuccess = () => {
+        setOpen(false);
+        console.log("edit form close on success triggered ", open);
+    };
+
+    const onCloseModal = () => setOpen(false);
+
+    const statusLabels = {
+        1: "Not Started",
+        2: "In Progress",
+        3: "Completed",
+    };
+
+    return (
+        <div>
+            <ul>
+                {tasks.map((task) => (
+                    <li key={task.id} className="task-card">
+                        Task {task.id}{" "}
+                        <button type="button" onClick={() => handleClick(task)}>
+                            Edit
+                        </button>
+                        <br />
+                        Progress Status: {statusLabels[task.status_id]}
+                        <br />
+                        Title: {task.title}
+                        <br />
+                        Category: {task.category}
+                        <br />
+                        Purpose: {task.purpose_description}
+                        <br />
+                        {task.due_date && task.due_date.length > 1 && (
+                            <>
+                                Goal Deadline:{" "}
+                                {format(
+                                    task.due_date,
+                                    "MMM dd, yyyy h:mma (z)"
+                                )}
+                            </>
+                        )}
+                        {task.completion_count >= 0 && (
+                            <>
+                                Weekly Completion Count: {task.completion_count}
+                            </>
+                        )}
+                        <br />
+                        Achievement Actions: {task.achievement_steps}
+                        <br />
+                    </li>
+                ))}
+            </ul>
+            <Modal open={open} onClose={onCloseModal}>
+                {open && selectedTask && (
+                    <EditAndReturnTask
+                        task={selectedTask}
+                        onClose={handleCloseOnSuccess}
+                    />
+                )}
+            </Modal>
+        </div>
+    );
+};
+
+export default FilteredTaskList;
--- frontend/src/child-components/SearchTaskByFilter.jsx
+++ frontend/src/child-components/SearchTaskByFilter.jsx
@@ -1,9 +1,159 @@
+import { useState } from "react";
+import FilteredTaskList from "./FilteredTaskList";
+
 const SearchTaskByFilter = () => {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [filteredTasks, setFilteredTasks] = useState({});
+    const [loadingMsg, setLoadingMsg] = useState("waiting for search...");
+
+    const [open, setOpen] = useState(false);
+    const [selectedTask, setSelectedTask] = useState(null);
+
+    const handleSubmit = async (event) => {
+        setLoadingMsg("Loading search data, please wait...");
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const columnToSearch = formData.get("column");
+        const value = formData.get("value");
+        const searchValue = String(value),
+            searchData = `search params sent: ${columnToSearch} ${searchValue}`;
+        console.log(searchData);
+
+        const user_id = localStorage.getItem("loginId");
+
+        try {
+            const url = `${
+                import.meta.env.VITE_SOURCE_URL
+            }/tasks/filter/${user_id}?column=${columnToSearch}&value=${searchValue}`;
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem(
+                        "accessToken"
+                    )}`,
+                },
+            }).then((response) => response.json());
+
+            console.log("search response:", response);
+            setIsLoading(false);
+            setFilteredTasks(response);
+            return;
+        } catch (error) {
+            console.error("Search ERROR: ", error);
+            setIsLoading(false);
+            setErrorMessage(`${error.error_detail}`);
+            console.log(errorMessage);
+            return;
+        }
+    };
+
+    const statusOptions = [
+        { value: 1, label: "Not Started" },
+        { value: 2, label: "In Progress" },
+        { value: 3, label: "Completed" },
+    ];
+    const columnOptions = [
+        { value: "title", label: "title" },
+        { value: "category", label: "category" },
+        { value: "status_id", label: "progress status" },
+    ];
+
+    const categoryOptions = [
+        { value: "Mental Health", label: "Mental Health" },
+        { value: "Physical Health", label: "Physical Health" },
+        { value: "Social Health", label: "Social Health" },
+        { value: "Organization", label: "Organization" },
+        { value: "Productivity", label: "Productivity" },
+        { value: "Skill Development", label: "Skill Development" },
+        { value: "Career", label: "Career" },
+        { value: "Finances", label: "Finances" },
+    ];
+
+    const [selectedColumn, setSelectedColumn] = useState("");
+    const handleChange = (event) => {
+        setSelectedColumn(event.target.value);
+    };
+
     return (
         <>
-            <p>
-                This page will allow you to filter your tasks by dropdown.
-            </p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="column-select">
+                    Choose a column to filter by:
+                </label>
+                <select
+                    name="column"
+                    id="column-select"
+                    onChange={handleChange}
+                >
+                    {columnOptions.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+                <label htmlFor="value">
+                    {selectedColumn === "title" ? (
+                        <>
+                            {" "}
+                            Enter Title:
+                            <input
+                                id="value"
+                                name="value"
+                                type="text"
+                                placeholder="existing title"
+                                required
+                            />
+                        </>
+                    ) : selectedColumn === "category" ||
+                    selectedColumn === "status_id" ? (
+                        <>
+                            Choose a {selectedColumn} to filter by:
+                            <select name="value" id="value" required>
+                                {selectedColumn === "category"
+                                    ? categoryOptions.map((option) => (
+                                        <option
+                                            key={option.value}
+                                            value={option.value}
+                                        >
+                                            {option.label}
+                                        </option>
+                                    ))
+                                    : statusOptions.map((option) => (
+                                        <option
+                                            key={option.value}
+                                            value={option.value}
+                                        >
+                                            {option.label}
+                                        </option>
+                                    ))}
+                            </select>
+                        </>
+                    ) : null}
+                </label>
+                <button type="submit">Search</button>
+                {isLoading ? (
+                    <p className="loading-message">{loadingMsg}</p>
+                ) : filteredTasks.length > 0 ? (
+                    <div>
+                        <h2>Search Results</h2>
+                        <FilteredTaskList
+                            tasks={filteredTasks}
+                            open={open}
+                            setOpen={setOpen}
+                            setSelectedTask={setSelectedTask}
+                            selectedTask={selectedTask}
+                        />{" "}
+                        {/* Pass state(s) as props */}
+                    </div>
+                ) : (
+                    <p>Sorry, no matches were found for your search criteria 🫤 <br/> Please try again..</p>
+                )}
+                {errorMessage && (
+                    <p className="error-message">{errorMessage}</p>
+                )}
+            </form>
         </>
     );
 };
